93fb465f8e68dfc30a416aabb358fb53d6f4a1b0
mariejobegin <petite.chikita@gmail.com>
2014-09-23 11:07:27 -0400

    deuxieme version

--- bin/ex04.dart
+++ bin/ex04.dart
@@ -2,65 +2,67 @@ import 'dart:math' as math;
 
 void main() {
    
-  DateTime dateNaissance;
-  
   // Question 1
-  Fibonacci(10);
-
-  // Question 2
-  CoordonneeRectangle(800, 600, 160, 45);
-  
-  // Question 3
-  dateNaissance = new DateTime(1988,7,1,0,0,0);
-  Age(dateNaissance);
-  
-  // Question 4
-  Interet(1000.0, 5, 0.048);
-  
-  // Question 5
-  TableDe8(16);
-  
-}
-
-void Fibonacci(int nombreTermes){
-  
-  int compteur;
-  List fibonacci;
-  int n1, n2;
-  
-  fibonacci = new List(nombreTermes);
-  
   print("");
   print("Question 1:");
-   
-  fibonacci[0] = 0;
-  fibonacci[1] = 1;
   
-  for (compteur = 0; compteur < nombreTermes; compteur++){
-    
-    if (compteur == 0 || compteur == 1 ){
-      print("f(" + compteur.toString() + ") = " + fibonacci[compteur].toString());
-    }
-    else{
-      n1 = fibonacci[compteur - 1];
-      n2 = fibonacci[compteur - 2];
-      
-      fibonacci[compteur] = n1 + n2;
-       print("f(" + compteur.toString() + ") = " + fibonacci[compteur].toString());
-    }
-    
-  }
-  
+  //Référence: https://docs.google.com/presentation/d/1dnz-3b4-KsdgTnLDzI381SILc26fCgyuuYLbe96gBC0/edit#slide=id.g2e60941c_0_20, diapositive 43
+  int count = 9;
+  String sequence = fibonacciIteration(count);
+  print('Fibonacci with iteration: '
+        '${count + 1} numbers sequence');
+  print(sequence);
+
+  // Question 2
   print("");
+  print("Question 2:");
+  print(CoordonneeRectangle(800, 600, 160, 45));
+  
+  // Question 3
+  print("");
+  print("Question 3:");
+  DateTime dateNaissance;
+  dateNaissance = new DateTime(1988,7,1,0,0,0);
+  print(Age(dateNaissance));
+  
+  // Question 4
+  print("");
+  print("Question 4:");
+  print(Interet(1000.0, 5, 0.048));
+  
+  // Question 5
+  print("");
+  print("Question 5:");
+  print(TableDe8(16));
   
 }
 
-void CoordonneeRectangle(int longueurRectangle, int hauteurRectangle, int deplacement, int angle) {
+//Référence: https://docs.google.com/presentation/d/1dnz-3b4-KsdgTnLDzI381SILc26fCgyuuYLbe96gBC0/edit#slide=id.g2e60941c_0_20, diapositive 42
+String fibonacciIteration(int t) {
+  
+  int a = 0, b = 1, f = 1, n = 1;
+  String result = '0' ' 1';
+  
+  while (n < t) {
+    f = a + b;
+    result = '$result $f';
+    a = b; 
+    b = f;
+    n = n + 1;
+  }
+  
+  return result;
+  
+}
+
+String CoordonneeRectangle(int longueurRectangle, int hauteurRectangle, int deplacement, int angle) {
     
   double xPointCentral;
   double yPointCentral;
   
   double x, y;
+  
+  String resultat;
   
   xPointCentral = longueurRectangle / 2;
   yPointCentral = hauteurRectangle / 2;
@@ -68,17 +70,18 @@ void CoordonneeRectangle(int longueurRectangle, int hauteurRectangle, int deplac
   x = math.sin(angle) * deplacement + xPointCentral;
   y = math.cos(angle) * deplacement + yPointCentral;
   
-  print("Question 2:");
-  print("(" + x.toStringAsFixed(2) + ", " + y.toStringAsFixed(2) + ")");
-  print("");
+  resultat = "(" + x.toStringAsFixed(2) + ", " + y.toStringAsFixed(2) + ")";
+  
+  return resultat;
   
 }
 
-void Age(DateTime dateNaissance){
+String Age(DateTime dateNaissance){
   
   DateTime maintenant;
   Duration duree;
   int annee, mois, jour;
+  String resultat;
 
   maintenant = new DateTime.now();
   maintenant = maintenant.subtract(new Duration(hours:maintenant.hour, minutes:maintenant.minute,seconds:maintenant.second, milliseconds:maintenant.millisecond));
@@ -89,39 +92,39 @@ void Age(DateTime dateNaissance){
   mois = maintenant.month - dateNaissance.month;
   jour = maintenant.day - dateNaissance.day;
 
-  print("Question 3:");
-  print(annee.toString() + " années " + mois.toString() + " mois " + jour.toString() + " jour");
-  print("");
-  
+  resultat = annee.toString() + " années " + mois.toString() + " mois " + jour.toString() + " jour";
+
+  return resultat;
+ 
 }
 
-void Interet(double somme, int annee, double tauxInteret){
+String Interet(double somme, int annee, double tauxInteret){
   
   int compteur;
-  
-  print("Question 4:");
-  
+  String resultat;
+    
   for (compteur = 1; compteur <= annee; compteur++){
     
     somme = somme * (1+tauxInteret);
-    print("Somme après " + compteur.toString() + " année(s): " + somme.toStringAsFixed(2));
+    resultat = "Somme après " + compteur.toString() + " année(s): " + somme.toStringAsFixed(2) + " \$";
     
   }
   
-  print("");
+  return resultat;
   
 }
 
-void TableDe8(int nombreTermes){
+String TableDe8(int nombreTermes){
   
   int compteur;
-  
-  print("Question 5:");
-  
+  String resultat= "";
+
   for (compteur = 1; compteur <= nombreTermes; compteur++){
       
-    print("8 x " + compteur.toString() + " = " + (8 * compteur).toString());
+    resultat = "$resultat 8 x " + compteur.toString() + " = " + (8 * compteur).toString() + "\n";
     
   }
+  
+  return resultat;
   
 }
